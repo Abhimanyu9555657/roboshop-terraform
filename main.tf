3ad55b5921e7c001c10b893a907df45f43c75483
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-30 16:41:56 +0530

    Terraform Basics

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-module "components" {
+module "vpc" {
   source = "git::https://github.com/Abhimanyu9555657/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+
+}
+
+output "vpc" {
+  value = module.vpc
 }
 
 
