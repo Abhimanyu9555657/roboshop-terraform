ce3e235e65ea533b07ea579b12a3aabf0be03873
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-01 00:33:20 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -5,8 +5,11 @@ module "vpc" {
   cidr     = each.value["cidr"]
   subnets  = each.value["subnets"]
   default_vpc_id = var.default_vpc_id
-
+  default_vpc_cidr = vpc.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
 }
+
+
 
 output "vpc" {
   value = module.vpc
