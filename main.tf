2177a7222cefd31d445a82a88f191ae5bde89092
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-31 14:12:49 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source = "git::https://github.com/Abhimanyu9555657/tf-module-vpc.git"
+  source   = "git::https://github.com/Abhimanyu9555657/tf-module-vpc.git"
 
   for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
+  cidr     = each.value["cidr"]
+  subnets  = each.value["subnets"]
 }
 
 output "vpc" {
