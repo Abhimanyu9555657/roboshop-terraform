6d9803031becad4097aa9a28fa11db0801eafaf0
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-03 15:13:40 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -8,13 +8,22 @@ module "vpc" {
   default_vpc_cidr           = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
   tags                       = var.tags
-  env = var.env
+  env                        = var.env
 }
 
+module "alb" {
+  source                     = "git::https://github.com/Abhimanyu9555657/tf-module-vpc.git"
 
-
-output "vpc" {
-  value = module.vpc
+  for_each                   = var.alb
+  internal                   = each.value["internal"]
+  lb_type                    = each.value["lb_type"]
+  sg_ingress_cidr            = each.value["sg_ingress_cidr"]
+  vpc_id                     = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+  subnets                    = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+  tags                       = var.tags
+  env                        = var.env
+  sg_port                    = each.value["sg_port"]
 }
+
 
 
