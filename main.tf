5c702f8618702dcab3e09b7c32a07fb7d2fb4126
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-06 14:43:14 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -55,12 +55,12 @@ module "rds" {
   subnet_ids              = local.db_subnets
   vpc_id                  = local.vpc_id
   sg_ingress_cidr         = local.app_subnets_cidr
-  rds_type = each.value["rds_type"]
-  db_port = each.value["db_port"]
-  engine_family = each.value["engine_family"]
-  engine = each.value["engine"]
-  engine_version = each.value["engine_version"]
+  rds_type                = each.value["rds_type"]
+  db_port                 = each.value["db_port"]
+  engine_family           = each.value["engine_family"]
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
 }
