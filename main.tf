779c47be0ad7cdf61076e796427ce343c154c463
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2024-01-07 18:32:11 +0530

    Creating Internet Gateway

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ module "rds" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
   instance_count          = each.value["instance_count"]
-  instance_class          = each.value["instance_class "]
+  instance_class          = each.value["instance_class"]
 }
 
 module "elasticache" {
